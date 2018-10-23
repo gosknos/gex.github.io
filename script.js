83460bc04270724a3e0d4c572812b4822bf052e9
gosknos <gxcharlotte@gmail.com>
2018-10-23 11:08:22 +0200

    task2

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ function greet() {
 
 function init() {
     document.getElementById("first").src = "flower2.png"; // Of course you can choose other file than flower2
+    document.getElementById("container").getElementsByTagName("img")[1].src = "flower2.png";
 
 }
