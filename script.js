e3772a7c91e97a089ff237be4e5c44c1eb492a61
gosknos <gxcharlotte@gmail.com>
2018-10-23 10:57:49 +0200

    task1

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function greet() {
     window.alert("Hello everyone !");
+}
+
+function init() {
+    document.getElementById("first").src = "flower2.png"; // Of course you can choose other file than flower2
 
 }
